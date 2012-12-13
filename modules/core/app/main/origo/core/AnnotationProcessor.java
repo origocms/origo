a80e522bf52c071a79cb90758343a46f8a6ed039
Jens Alm <jens@ulmus.org>
2012-12-13 16:46:45 +0100

    Annotation scan. Default theme's templates need to be updated.

--- modules/core/app/main/origo/core/AnnotationProcessor.java
+++ modules/core/app/main/origo/core/AnnotationProcessor.java
@@ -1,7 +1,9 @@
 package main.origo.core;
 
+import com.google.common.base.Predicates;
 import main.origo.core.annotations.*;
 import org.apache.commons.lang3.StringUtils;
+import org.reflections.ReflectionUtils;
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
@@ -15,6 +17,7 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -47,47 +50,51 @@ public class AnnotationProcessor {
     }
 
     private static void scan(String filter) {
-        Reflections reflections = new Reflections(new ConfigurationBuilder().filterInputsBy(FilterBuilder.parse(filter)).setUrls(ClasspathHelper.forClassLoader()));
+        Reflections reflections = new Reflections("");
 
-        scanInterceptors(reflections, Provides.class, void.class, Provides.Context.class);
-        scanInterceptors(reflections, OnLoad.class, void.class, OnLoad.Context.class);
+        Set<Class<?>> interceptors = reflections.getTypesAnnotatedWith(Interceptor.class);
+        scanInterceptors(interceptors, Provides.class, void.class, Provides.Context.class);
+        scanInterceptors(interceptors, OnLoad.class, void.class, OnLoad.Context.class);
 
-        scanThemes(reflections, Theme.class);
+        Set<Class<?>> themes = reflections.getTypesAnnotatedWith(Theme.class);
+        scanThemes(themes);
 
-        scanThemesParts(reflections, Decorates.class, String.class, Decorates.Context.class, new AddThemepartFunction() {
-            @Override
-            public void add(Theme theme, Annotation annotation, Class declaringClass, MethodHandle methodHandle) {
-                ThemeVariant themeVariant = (ThemeVariant) annotation;
-                Themes.addThemeVariant(theme.id(), themeVariant.id(), themeVariant.regions(), declaringClass, methodHandle);
-            }
-        });
-        scanThemesParts(reflections, ThemeVariant.class, Result.class, ThemeVariant.Context.class, new AddThemepartFunction() {
+        scanThemeParts(themes, Decorates.class, String.class, Decorates.Context.class, new AddThemePartFunction() {
             @Override
             public void add(Theme theme, Annotation annotation, Class declaringClass, MethodHandle methodHandle) {
                 Decorates decorates = (Decorates) annotation;
                 Themes.addDecorator(theme.id(), decorates.type(), declaringClass, methodHandle);
             }
         });
+        scanThemeParts(themes, ThemeVariant.class, Result.class, ThemeVariant.Context.class, new AddThemePartFunction() {
+            @Override
+            public void add(Theme theme, Annotation annotation, Class declaringClass, MethodHandle methodHandle) {
+                ThemeVariant themeVariant = (ThemeVariant) annotation;
+                Themes.addThemeVariant(theme.id(), themeVariant.id(), themeVariant.regions(), declaringClass, methodHandle);
+            }
+        });
     }
 
-    private static void scanInterceptors(Reflections reflections, Class<? extends Annotation> annotationClass, Class returnType, Class contextClass) {
-        Set<Method> methods = reflections.getMethodsAnnotatedWith(annotationClass);
+    private static void scanInterceptors(Set<Class<?>> classes, Class<? extends Annotation> annotationClass, Class returnType, Class contextClass) {
         MethodHandles.Lookup lookup = MethodHandles.lookup();
-        for (Method m : methods) {
-            MethodType methodType = MethodType.methodType(returnType, contextClass);
-            try {
-                MethodHandle methodHandle = lookup.findStatic(m.getDeclaringClass(), m.getName(), methodType);
-                Listeners.addListener(m.getAnnotation(annotationClass), m.getDeclaringClass(), methodHandle);
-            } catch (NoSuchMethodException | IllegalAccessException e) {
-                throw new InitializationException("Method '" + m.getDeclaringClass() + "." + m.getName() + "' in " +
-                        " is annotated with '" + annotationClass.getSimpleName() +
-                        "' but the method signature does not match the required Context '" + contextClass.getSimpleName() + "'", e);
+
+        for(Class c : classes) {
+            Set<Method> methods = Reflections.getAllMethods(c, ReflectionUtils.withAnnotation(annotationClass));
+            for (Method m : methods) {
+                MethodType methodType = MethodType.methodType(returnType, contextClass);
+                try {
+                    MethodHandle methodHandle = lookup.findStatic(m.getDeclaringClass(), m.getName(), methodType);
+                    Listeners.addListener(m.getAnnotation(annotationClass), m.getDeclaringClass(), methodHandle);
+                } catch (NoSuchMethodException | IllegalAccessException e) {
+                    throw new InitializationException("Method '" + m.getDeclaringClass() + "." + m.getName() + "' in " +
+                            " is annotated with '" + annotationClass.getName() +
+                            "' but the method signature does not match the required signature", e);
+                }
             }
         }
     }
 
-    private static void scanThemes(Reflections reflections, Class<? extends Annotation> annotationClass) {
-        Set<Class<?>> classes = reflections.getTypesAnnotatedWith(annotationClass);
+    private static void scanThemes(Set<Class<?>> classes) {
 
         for(Class c : classes) {
             Theme themeAnnotation = (Theme)c.getAnnotation(Theme.class);
@@ -95,34 +102,32 @@ public class AnnotationProcessor {
         }
     }
 
-    private static void scanThemesParts(Reflections reflections, Class<? extends Annotation> annotationClass, Class returnType, Class contextClass, AddThemepartFunction addThemepartFunction) {
+    private static void scanThemeParts(Set<Class<?>> classes, Class<? extends Annotation> annotationClass, Class returnType, Class contextClass, AddThemePartFunction addThemepartFunction) {
 
-        Set<Method> methods = reflections.getMethodsAnnotatedWith(annotationClass);
         MethodHandles.Lookup lookup = MethodHandles.lookup();
 
-        for (Method m : methods) {
+        for(Class c : classes) {
+            Set<Method> methods = Reflections.getAllMethods(c, ReflectionUtils.withAnnotation(annotationClass));
 
-            if (!m.getDeclaringClass().isAnnotationPresent(Theme.class)) {
-                throw new InitializationException("Method '" + m.getDeclaringClass() + "." + m.getName() +
-                        "' is annotated with '" + annotationClass.getSimpleName() +
-                        "' but the class is not annotated with the required @Theme");
-            }
+            Theme themeAnnotation = (Theme)c.getAnnotation(Theme.class);
 
-            Theme themeAnnotation = m.getDeclaringClass().getAnnotation(Theme.class);
-            MethodType methodType = MethodType.methodType(returnType, contextClass);
-            try {
-                MethodHandle methodHandle = lookup.findStatic(m.getDeclaringClass(), m.getName(), methodType);
-                Annotation annotation = m.getAnnotation(annotationClass);
-                addThemepartFunction.add(themeAnnotation, annotation, m.getDeclaringClass(), methodHandle);
-            } catch (NoSuchMethodException | IllegalAccessException e) {
-                throw new InitializationException("Method '" + m.getDeclaringClass() + "." + m.getName() +
-                        "' is annotated with '" + annotationClass.getSimpleName() +
-                        "' but the method signature does not match the required Context '" + contextClass.getSimpleName() + "'", e);
+            for (Method m : methods) {
+
+                MethodType methodType = MethodType.methodType(returnType, contextClass);
+                try {
+                    MethodHandle methodHandle = lookup.findStatic(c, m.getName(), methodType);
+                    Annotation annotation = m.getAnnotation(annotationClass);
+                    addThemepartFunction.add(themeAnnotation, annotation, c, methodHandle);
+                } catch (NoSuchMethodException | IllegalAccessException e) {
+                    throw new InitializationException("Method '" + m.getDeclaringClass() + "." + m.getName() +
+                            "' is annotated with '" + annotationClass.getName() +
+                            "' but the method signature does not match the required signature", e);
+                }
             }
         }
     }
 
-    private static interface AddThemepartFunction {
+    private static interface AddThemePartFunction {
         public void add(Theme theme, Annotation annotation, Class declaringClass, MethodHandle methodHandle);
     }
 
--- modules/core/app/main/origo/core/themes/DefaultTheme.java
+++ modules/core/app/main/origo/core/themes/DefaultTheme.java
@@ -3,18 +3,22 @@ package main.origo.core.themes;
 import main.origo.core.annotations.Theme;
 import main.origo.core.annotations.ThemeVariant;
 import main.origo.core.ui.RenderedNode;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.origo.core.themes.DefaultTheme.variant_main_and_left_columns;
+import views.html.origo.core.themes.DefaultTheme.variant_three_columns;
 
 @Theme(id = "default")
 public class DefaultTheme {
 
     @ThemeVariant(id = "default-main_and_left_columns", regions = {"main", "left"})
-    public String getDefaultMainAndLeftColumnTemplate(RenderedNode renderedNode) {
-        return "themes/origo/DefaultTheme/variant_main_and_left_columns.html";
+    public static Result getDefaultMainAndLeftColumnTemplate(ThemeVariant.Context context) {
+        return Controller.ok(variant_main_and_left_columns.render(context));
     }
 
     @ThemeVariant(id = "default-three_columns", regions = {"main", "left", "right"})
-    public String getDefaultThreeColumnTemplate(RenderedNode renderedNode) {
-        return "themes/origo/DefaultTheme/variant_three_columns.html";
+    public static Result getDefaultThreeColumnTemplate(ThemeVariant.Context context) {
+        return Controller.ok(variant_three_columns.render(context));
     }
 
 }
