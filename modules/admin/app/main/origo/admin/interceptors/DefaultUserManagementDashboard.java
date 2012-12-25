6ae2f3ce85e4c9cf366282f526e2a26e8714513d
Jens Alm <jens@ulmus.org>
2012-12-25 19:26:29 +0100

    [admin] Moved user management dashboard to it's own file. BasicUserAdminProvider will be a simple user management tool.

---
+++ modules/admin/app/main/origo/admin/interceptors/DefaultUserManagementDashboard.java
@@ -0,0 +1,64 @@
+package main.origo.admin.interceptors;
+
+import main.origo.admin.annotations.Admin;
+import main.origo.admin.helpers.AdminHelper;
+import main.origo.admin.helpers.DashboardHelper;
+import main.origo.core.Node;
+import main.origo.core.NodeLoadException;
+import main.origo.core.annotations.*;
+import main.origo.core.ui.Element;
+import models.origo.admin.AdminPage;
+import models.origo.core.RootNode;
+
+import java.util.List;
+
+@Interceptor
+public class DefaultUserManagementDashboard {
+
+    /*
+     * Dashboard Item for front page.
+     */
+    @Provides(type = Admin.DASHBOARD_ITEM, with = Admin.USER_PAGE_TYPE)
+    @Relationship(parent = Admin.FRONT_PAGE_TYPE)
+    public static Element addUserDashboardItemToFrontPage(Provides.Context context) {
+        return AdminHelper.createBasicDashboardItem().
+                addChild(new Element.Panel().setWeight(20).
+                        addChild(new Element.Heading4().setWeight(10).setBody("User Management").addAttribute("class", "title")).
+                        addChild(new Element.Anchor().setWeight(10).setBody("View").addAttribute("href", DashboardHelper.getDashBoardURL(Admin.USER_PAGE_TYPE))));
+    }
+
+    /*
+     * Creating the Node for the User Dashboard.
+     */
+    @Provides(type = Types.NODE, with = Admin.USER_PAGE_TYPE)
+    public static Node createUserDashboard(Provides.Context context) throws NodeLoadException {
+        AdminPage page = new AdminPage(context.node.getNodeId());
+        page.setTitle("User Management - Dashboard");
+        page.rootNode = (RootNode) context.node;
+
+        page.addUIElement(DashboardHelper.createDashboard(Admin.USER_PAGE_TYPE, page));
+
+        return page;
+    }
+
+    /*
+     * Creating the Dashboard for the Node created above.
+     */
+    @Provides(type = Admin.DASHBOARD, with = Admin.USER_PAGE_TYPE)
+    @Relationship(parent = Admin.FRONT_PAGE_TYPE)
+    public static Element addUserDashboardContent(Provides.Context context) {
+        return AdminHelper.createBasicDashboard();
+    }
+
+    /*
+     * Creating the Dashboard Items for the User dashboard.
+    */
+    @OnLoad(type = Admin.DASHBOARD, with = Admin.USER_PAGE_TYPE)
+    public static void addFrontPageDashboardItems(OnLoad.Context context) {
+        List<Element> elements = DashboardHelper.createDashboardItems(Admin.USER_PAGE_TYPE, context.node);
+        for(Element element : elements) {
+            context.node.addUIElement(element);
+        }
+    }
+
+}
