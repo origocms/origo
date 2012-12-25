9e36c708fc38dcc7dc9937d9bc6ccee0adba9ce9
Jens Alm <jens@ulmus.org>
2012-12-25 11:39:08 +0100

    [core] Refactored WysiHtml editor.

--- modules/core/app/main/origo/core/interceptors/forms/WysiHTML5EditorProvider.java
+++ modules/core/app/main/origo/core/interceptors/forms/WysiHTML5EditorProvider.java
@@ -6,22 +6,35 @@ import main.origo.core.ui.Element;
 import main.origo.core.ui.RenderingContext;
 import models.origo.core.Content;
 import play.api.templates.Html;
-import views.html.origo.core.decorators.forms.wysi.wysihtml5;
+import views.html.origo.core.decorators.forms.wysi.wysihtml5_script;
+import views.html.origo.core.decorators.forms.wysi.wysihtml5_toolbar;
 
 @Interceptor
 public class WysiHTML5EditorProvider {
 
     public static final String EDITOR_TYPE = "origo.admin.editor.wysihtml5";
 
-    public static class WysiHtml5EditorElement extends Element {
+    public static class WysiHtml5ToolbarElement extends Element {
 
-        public WysiHtml5EditorElement() {
-            super("wysihtml5_editor");
+        public WysiHtml5ToolbarElement() {
+            super("wysihtml5_editor_toolbar");
         }
 
         @Override
         public Html decorate(RenderingContext renderingContext) {
-            return wysihtml5.render(this.id);
+            return wysihtml5_toolbar.render(this.id);
+        }
+    }
+
+    public static class WysiHtml5ScriptElement extends Element {
+
+        public WysiHtml5ScriptElement() {
+            super("wysihtml5_editor_script");
+        }
+
+        @Override
+        public Html decorate(RenderingContext renderingContext) {
+            return wysihtml5_script.render(this.id);
         }
     }
 
@@ -35,19 +48,12 @@ public class WysiHTML5EditorProvider {
 
     @OnInsertElement(with = Element.InputTextArea.class)
     public static void insertToolbar(OnInsertElement.Context context) {
-        context.parent.addChild(new WysiHtml5EditorElement().setId(context.element.id));
+        context.parent.addChild(new WysiHtml5ToolbarElement().setId(context.element.id));
     }
 
     @OnInsertElement(with = Element.InputTextArea.class, after = true)
     public static void insertScript(OnInsertElement.Context context) {
-        context.parent.addChild(new Element.Script().addAttribute("type", "text/javascript").
-                setBody(
-                        "  new wysihtml5.Editor(\"" + context.element.id + "\", {\n" +
-                                "      toolbar:        \"toolbar-" + context.element.id + "\",\n" +
-                                //"      stylesheets:    \"css/stylesheet.css\",\n" +
-                                "      parserRules:    wysihtml5ParserRules\n" +
-                                "  });\n"
-                ));
+        context.parent.addChild(new WysiHtml5ScriptElement().setId(context.element.id));
     }
 
     @Provides(type = Types.RICHTEXT_EDITOR, with = EDITOR_TYPE)
