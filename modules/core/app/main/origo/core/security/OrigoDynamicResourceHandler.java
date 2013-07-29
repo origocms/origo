493f065a4e4a0c27b66317a732af776e943a9cda
Jens Alm <jens@ulmus.org>
2013-07-28 21:57:52 -0400

    Added resource parameters to args for Provides event generator.

--- modules/core/app/main/origo/core/security/OrigoDynamicResourceHandler.java
+++ modules/core/app/main/origo/core/security/OrigoDynamicResourceHandler.java
@@ -2,6 +2,7 @@ package main.origo.core.security;
 
 import be.objectify.deadbolt.java.DeadboltHandler;
 import be.objectify.deadbolt.java.DynamicResourceHandler;
+import com.google.common.collect.Maps;
 import main.origo.core.ModuleException;
 import main.origo.core.NodeLoadException;
 import main.origo.core.annotations.Core;
@@ -9,12 +10,20 @@ import main.origo.core.event.ProvidesEventGenerator;
 import play.Logger;
 import play.mvc.Http;
 
+import java.util.Map;
+
 public class OrigoDynamicResourceHandler implements DynamicResourceHandler {
+
+    public static final String AUTH_HANDLER = "AUTHORIZATION_HANDLER";
+    public static final String AUTH_META = "AUTHORIZATION_META";
 
     @Override
     public boolean isAllowed(String name, String meta, DeadboltHandler deadboltHandler, Http.Context context) {
         try {
-            return ProvidesEventGenerator.triggerInterceptor(null, Core.Type.USER, Core.With.AUTHORIZATION_CHECK);
+            Map<String, Object> args = Maps.newHashMap();
+            args.put(AUTH_HANDLER, name);
+            args.put(AUTH_META, meta);
+            return ProvidesEventGenerator.triggerInterceptor(null, Core.Type.USER, Core.With.AUTHORIZATION_CHECK, args);
         } catch (NodeLoadException | ModuleException e) {
             Logger.error("Authorization check failed, returning false", e);
             return false;
